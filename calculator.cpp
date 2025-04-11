e7525fd3ba51bbf016c370e00108aea915f0ea01
khk <khkryan0905@gmail.com>
2025-04-11 11:54:14 +0900

    implementation

--- calculator.cpp
+++ calculator.cpp
@@ -1,18 +1,40 @@
 #include "calculator.h"
 #include <stdexcept>
 
-int Calculator::add(int a, int b) {
-    // TODO
+int Calculator::add(int a, int b)
+{
+    if ((b > 0 && a > INT_MAX - b) ||
+        (b < 0 && a < INT_MIN - b))
+    {
+        throw std::overflow_error("overflow/underflow");
+    }
+    return a + b;
 }
 
-int Calculator::sub(int a, int b) {
-    // TODO
+int Calculator::sub(int a, int b)
+{
+    if ((b < 0 && a > INT_MAX - b) ||
+        (b > 0 && a < INT_MIN + b))
+    {
+        throw std::overflow_error("overflow/underflow");
+    }
+    return a - b;
 }
 
-int Calculator::mul(int a, int b) {
-    // TODO
+int Calculator::mul(int a, int b)
+{
+    if ((a > INT_MAX / b) || (a < INT_MIN / b))
+    {
+        throw std::overflow_error("overflow/underflow");
+    }
+    return a * b;
 }
 
-int Calculator::div(int a, int b) {
-    // TODO
+int Calculator::div(int a, int b)
+{
+    if (b == 0)
+        throw std::invalid_argument("Division by zero");
+    if (a == INT_MIN && b == -1)
+        throw std::overflow_error("overflow/underflow");
+    return a / b;
 }
